60a27aba3468f7ed3cf5832519db58e828fb9870
Jessica Whitney <jessica.whitney.22@ucl.ac.uk>
2024-09-16 23:02:51 +0100

    Adds empty urls and views

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    # make views for following
+    path("listing/<int:id>", views.listing, name="listing"),
+    path("categories", views.categories, name="categories"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("newListing", views.new_listing, name="new_listing"),
 ]
